efd74c2ece9dff705460be595e7192d77e69d015
Thorayush <gursalayush@gmail.com>
2024-04-03 15:28:41 +0530

    Updated code for 3 commit

--- App.py
+++ App.py
@@ -3,7 +3,7 @@ from flask import *
 from i2p import i2pconverter
 app = Flask(__name__)
 
-@app.route('/Img2Pdf')
+@app.route('/')
 def index():
     return render_template('index.html')
 
@@ -21,4 +21,4 @@ def download():
     filename = f1.split('.')[0]+'converted.pdf'
     return send_file(filename,as_attachment=True)
 
-app.run(debug=True)
+app.run(debug=False)
